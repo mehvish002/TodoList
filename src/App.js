e03d5903da425dfbc6680a7e297a5e1653caf323
mehvish002 <mehvish0882.be21@chitkara.edu.in>
2023-10-26 07:30:58 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -34,6 +34,8 @@ const App = () => {
       setTodo("")
     }
   };
+
+
   const handleDelete=(id)=>{
     const delTodo=todos.filter((to)=>to.id !==id);
     setTodos([...delTodo]);
@@ -60,6 +62,7 @@ const App = () => {
       todos.map((t)=>(
         <li className="singleTodo">
           <span className="todoText" key={t.id}>{t.todo}</span>
+         
           <button onClick={()=>handleEdit(t.id)}>Edit</button>
           <button onClick={()=>handleDelete(t.id)}>Delete</button>
         </li>
